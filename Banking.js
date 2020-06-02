3a148e1c45b7c3a28d9f569b7aa0933fe3eaa5b3
jlb2886 <55092716+jlb2886@users.noreply.github.com>
2020-06-02 15:10:36 -0400

    JavaScript fix

--- Banking.js
+++ Banking.js
@@ -1,9 +1,27 @@
 // Get the modal
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+function validateForm() {
+
+    var x, y, text;
+
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("pass").value;
+    y = document.getElementById("uname").value;
+
+    // If x is Not a Number or less than one or greater than 10
+    if (x != "2020" && y != "2020" ) {
+        text = "Input not valid";
+    } else {
+        text = "Hi Cyber Geek you entered the correct username and password ;)";
     }
+    document.getElementById("valid").innerHTML = text;
+
 }
